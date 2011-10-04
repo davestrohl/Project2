6d6ff96851ae24a06e4097588d1bde92d9e8cc5c
Dave Strohl <strohd@rpi.edu>
2011-10-03 20:46:15 -0400

    changes in main.lua(?)

--- main.lua
+++ main.lua
@@ -4,12 +4,17 @@
 local physics = require("physics")
 local sprite = require("sprite")
 
+local enemy = require("enemy")
+
+local game = display.newGroup();
+game.x = 0
+
 physics.start()
 
 local background = display.newRect(0, 0, display.contentWidth, display.contentHeight)
 background:setFillColor(0, 0, 200)
 
---physics.setDrawMode("hybrid") --Set physics Draw mode
+physics.setDrawMode("hybrid") --Set physics Draw mode
 physics.setScale( 60 ) -- a value that seems good for small objects (based on playtesting)
 physics.setGravity( 0, 0 ) -- overhead view, therefore no gravity vector
 display.setStatusBar( display.HiddenStatusBar )
@@ -28,10 +33,41 @@ player.yScale = 0.5
 player:prepare("def")
 player:play()
 
+
+
 local playerBody = {density = 1.5, friction = 0.5, bounce = .3}
 physics.addBody(player, playerBody)
 
-local function playerTouch(event)
-    player:applyLinearImpulse((event.x - player.x) * .5, (event.y - player.y) * .5, player.x, player.y)
+--[[local circle = display.newCircle(enemy.x, enemy.y, 50)
+circle:setFillColor(0,255,0)
+circle.alpha = 0.4]]
+
+local enemyBody = {density = 1.5, friction = 0.7, bounce = 0.3, radius = 50, isSensor = true}
+
+local enemy1 = enemy.newEnemy(150, 150)
+local loc = enemy1:getLocation()
+game:insert(enemy1); enemy1.id = "enemy1"; enemy1.x = loc.x; enemy1.y = loc.y;
+physics.addBody(enemy1, enemyBody)
+
+local enemy2 = enemy.newEnemy(350, 350)
+game:insert(enemy2); enemy2.id = "enemy2"; enemy2.x = 350; enemy2.y = 350; 
+physics.addBody(enemy2, enemyBody)
+
+local function onCollide(event)
+	if((event.object1 == enemy1 or event.object2 == enemy1) and (event.object1 == player or event.object2 == player) and event.phase == "began") then
+		background:setFillColor(255,0,0)
+	elseif((event.object1 == enemy1 or event.object2 == enemy1) and (event.object1 == player or event.object2 == player) and event.phase == "ended") then
+		background:setFillColor(0,0,200)
+	end
 end
+
+local function playerTouch(event)
+    --player:applyLinearImpulse((event.x - player.x) * .5, (event.y - player.y) * .5, player.x, player.y)
+	player.x = event.x
+	player.y = event.y
+	enemy1:setLocation({x = enemy1.x + event.x, y = enemy1.y + event.y})
+end
+
 background:addEventListener("touch", playerTouch)
+
+Runtime:addEventListener("collision", onCollide)
