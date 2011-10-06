eb058a2d82cedf1b3d65ae60482dab8c939bab1b
Dave Strohl <strohd@rpi.edu>
2011-10-06 19:02:01 -0400

    altered main

--- enemy.lua
+++ enemy.lua
@@ -3,13 +3,14 @@
 module(..., package.seeall)
 
 local sprite = require("sprite")
-
-function newEnemy(x, y)
+local enemyBody = {density = 1.5, friction = 0.7, bounce = 0.3, radius = 50, isSensor = true}
+function newEnemy(x, y, p)
 	local g = display.newGroup()
 	local enemySheet = sprite.newSpriteSheet("ball_white.png", 72,72)
 	local enemySet = sprite.newSpriteSet(enemySheet, 1, 1)
 	sprite.add(enemySet, "patrol", 1, 1, 1000)
 	local enemy = sprite.newSprite(enemySet)
+	--local enemy = display.newImage("ball_white.png")
 	enemy.x = x
 	enemy.y = y
 	enemy.xScale = 0.5
@@ -18,6 +19,10 @@ function newEnemy(x, y)
     enemy:play()
 	--local object = {x = x, y = y, sprite = enemy}
 	--setmetatable(object, {__index = Enemy})
+	--p.addBody(enemy, enemyBody)
+	
+	--g:insert(enemy)
+	
 	
 	function g:getLocation()
 		return {x = x , y = y}
@@ -26,6 +31,11 @@ function newEnemy(x, y)
 	function g:setLocation( loc )
 		enemy.x = loc.x; enemy.y = loc.y
 	end
+	
+	function g:move(x , y)
+		enemy:applyForce(x, y, enemy.x, enemy.y)
+	end
+	
 	
 	return g
 end
