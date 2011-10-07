078beaf5fd13fe173e5d844ef118a1961c803c63
Dave Strohl <strohd@rpi.edu>
2011-10-06 21:04:19 -0400

    penguins and sentries in a box

--- Beth_code/level1map.lua
+++ Beth_code/level1map.lua
@@ -4,6 +4,7 @@ module(..., package.seeall)
 sprite = require("sprite")
 physics = require("physics")
 
+physics.setGravity( 0, 0 )
 --Declarations - variables/switches/etc
 --make world group
 local worldgroup=display.newGroup()
@@ -26,6 +27,183 @@ PLANT = "../gfx/filler_plant.png"
 --
 --Globals - to be accessed from main.lua
 callUnload = false
+
+local playerBody = {density = 1.5, friction = 0.5, bounce = .3}
+Player = {x = 0, y = 0, spr = nil, disguised = false}
+
+function Player:new(x, y)
+    self.x = x; self.y = y
+    
+    local playerSheet = sprite.newSpriteSheet("../gfx/mock.png", 72, 72)
+    local playerSet = sprite.newSpriteSet(playerSheet, 1, 25)
+    --Set up animations for all the costumes
+    sprite.add(playerSet, "def", 1, 5, 5000)
+    sprite.add(playerSet, "plant", 21, 5, 5000, -2)
+    sprite.add(playerSet, "security", 11, 5, 5000)
+    sprite.add(playerSet, "statue", 16, 5, 5000)
+    sprite.add(playerSet, "change", 6, 5, 1000, 1)
+    
+    local player = sprite.newSprite(playerSet)
+    player.x = x
+    player.y = y
+    
+    player:prepare("def")
+    player:play()
+    
+    self.spr = player
+    
+    local object = {x = x, y = y, spr = self.spr}
+    setmetatable(object, {__index = Player})
+    self.spr.super = object
+    return object
+    
+end
+
+function Player:pose(disguise)
+    self.spr:prepare(disguise)
+    self.spr:play()
+    self.disguised = true
+end
+
+function Player:unpose()
+    self.spr:prepare("def")
+    self.spr:play()
+    self.disguised = false
+end
+
+function Player:getLocation()
+	return {x = self.spr.x , y = self.spr.y}
+end
+	
+function Player:setLocation( loc )
+	self.spr.x = loc.x; self.spr.y = loc.y
+end
+
+local function playerTouch(self, event)
+    -- print(event.target)
+    -- print(player.spr)
+    local t = event.target
+    if event.phase == "began" then
+        display.getCurrentStage():setFocus(t)
+        t.isFocus = true
+        line = nil
+    elseif t.isFocus then
+        if event.phase == "moved" then
+            if ( line ) then
+				line.parent:remove( line ) -- erase previous line, if any
+			end
+			line = display.newLine( t.x,t.y, event.x,event.y )
+			line:setColor( 255, 255, 255, 50 )
+			line.width = 15
+        elseif (event.phase == "ended" or event.phase == "cancelled") then
+            display.getCurrentStage():setFocus(nil)
+            t.isFocus = false
+            if ( line ) then
+				line.parent:remove( line )
+			end
+            t:applyForce( (event.x - t.x), (event.y - t.y), t.x, t.y )	
+        end
+    end
+end
+
+viewRight = {0,0 , 80,-30 , 80,30}
+viewLeft = {0,0 , -80,30 , -80,-30}
+local enemyBodyLeft = {density = 1.5, friction = 0.7, bounce = 0.3, isSensor = true, shape = viewLeft}
+local enemyBodyRight = {density = 1.5, friction = 0.7, bounce = 0.3, isSensor = true, shape = viewRight}
+Enemy = { x = 0, y = 0 , spr = nil, initDirection = 'r', bound1 = nil, bound2 = nil}
+function Enemy:new(x, y)
+	self.x = x; self.y = y
+
+	
+	local enemySheet = sprite.newSpriteSheet("../ball_white.png", 72,72)
+	local enemySet = sprite.newSpriteSet(enemySheet, 1, 1)
+	sprite.add(enemySet, "patrol", 1, 1, 1000)
+	
+	local enemy = sprite.newSprite(enemySet)
+	enemy.x = x
+	enemy.y = y
+	enemy.xScale = 0.5
+	enemy.yScale = 0.5
+    enemy:prepare("patrol")
+    enemy:play()
+	
+	self.spr = enemy
+	self.spr.direction = self.initDirection
+	
+	local object = {x = x, y = y, spr = self.spr, direction = self.spr.direction}
+	setmetatable(object, {__index = Enemy})
+	self.spr.super = object
+	
+	return object
+end
+
+local function boundCollide(self, event)
+
+	if ((event.other == self.bound1 or event.other == self.bound2) and event.phase == "began") then
+					--print(self.super)
+					--print(self)
+					if(self.direction == 'l') then
+						self.direction = 'r'
+						self.super.spr.direction = 'r'
+					else
+						self.direction = 'l'
+						self.super.spr.direction = 'l'
+					end
+					--print(self.super.spr.direction)
+					self.super:patrol()
+				elseif ((event.other == self.bound1 or event.other == self.bound2) and event.phase == "ended") then
+					--self:applyAngularImpulse(10)	
+				end
+end
+
+function Enemy:spawnBounds( dist )
+	local bound1 = display.newRect(self.spr.x - dist.x, self.spr.y, 10, 10)
+	physics.addBody(bound1, "static", {bounce = 0.2, isSensor = true})
+	self.spr.bound1 = bound1
+	local bound2 = display.newRect(self.spr.x + dist.x, self.spr.y, 10, 10)
+	physics.addBody(bound2, "static", {bounce = 0.2, isSensor = true})
+	self.spr.bound2 = bound2
+	--[[local function boundCollide(self, event)
+			    
+				if ((event.other == self.bound1 or event.other == self.bound2) and event.phase == "began") then
+					--print(self.direction)
+					if(self.direction == 'l') then
+						self.direction = 'r'
+						self.super.spr.direction = 'r'
+					else
+						self.direction = 'l'
+						self.super.spr.direction = 'l'
+					end
+					--print(self.super.spr.direction)
+					self.super:patrol()
+				elseif ((event.other == self.bound1 or event.other == self.bound2) and event.phase == "ended") then
+				
+				
+				end
+		end]]
+	--self.spr.collision = boundCollide
+
+	--self.spr:addEventListener("collision", self.spr)
+end
+
+function Enemy:getLocation()
+	return {x = self.spr.x , y = self.spr.y}
+end
+	
+function Enemy:setLocation( loc )
+	self.spr.x = loc.x; self.spr.y = loc.y
+end
+
+--function Enemy:get
+
+function Enemy:patrol()
+	if(self.spr.direction == 'l') then
+		self.spr:setLinearVelocity(-30, 0)
+		--self.spr:applyLinearImpulse(math.random(-500, 500), math.random(-500, 500), self.spr.x + math.random(-20, 20), self.spr.y + math.random(-20, 20))
+	else
+		self.spr:setLinearVelocity(30, 0)
+	end
+end
 
 --Creation Functions
 --[[
@@ -93,7 +271,9 @@ end
 
 init=function()
     physics.start()
-    --physics.setDrawMode("hybrid")
+    physics.setDrawMode("hybrid")
+	physics.setGravity( 0, 0 )
+
     --put invisible walls around the world
     top = display.newRect(0,0, 1056, 0)
     physics.addBody(top, "static", {bounce =1})
@@ -104,7 +284,7 @@ init=function()
     left = display.newRect(0,0,0,960)
     physics.addBody(left, "static", {bounce =1})
     worldgroup:insert(left)
-    right = display.newRect(0,1056,0,960)
+    right = display.newRect(1056,0,0,960)
     physics.addBody(right, "static", {bounce =1})
     worldgroup:insert(right)
     --input penguin for testing purposes
@@ -119,6 +299,38 @@ init=function()
     penguinpoly = {-30, -70, 30, -70, 70, 0, 40, 60, -40, 60, -70, 0}
     physics.addBody(penguin, {density = 1.0, friction =10, bounce = 1, shape=penguinpoly})
     worldgroup:insert(penguin)
+	
+	local player = Player:new(250, 250)
+	physics.addBody(player.spr, playerBody)
+	player.spr.linearDamping = .8
+
+
+	local enemy1 = Enemy:new(150, 150)
+	local loc = enemy1:getLocation()
+	physics.addBody(enemy1.spr, enemyBodyRight)
+	enemy1.spr.angularDamping = 2
+	enemy1:spawnBounds({x = 125, y = 0})
+	enemy1.spr.collision = boundCollide
+	enemy1.spr:addEventListener("collision", enemy1.spr)
+	enemy1:patrol()
+
+
+	local enemy2 = Enemy:new(350, 350)
+	physics.addBody(enemy2.spr, enemyBodyRight)
+	enemy2.spr.angularDamping = 1
+	enemy2:spawnBounds({x = 100, y = 0})
+	enemy2.spr.collision = boundCollide
+	enemy2.spr:addEventListener("collision", enemy2.spr)
+	enemy2:patrol()
+	
+	worldgroup:insert(player.spr)
+	worldgroup:insert(enemy1.spr)
+	worldgroup:insert(enemy1.spr.bound1)
+	worldgroup:insert(enemy1.spr.bound2)
+	worldgroup:insert(enemy2.spr)
+	worldgroup:insert(enemy2.spr.bound1)
+	worldgroup:insert(enemy2.spr.bound2)
+	
     --map initialization
     --read in from file
     local path = system.pathForFile("test.txt", system.ResourceDirectory)
@@ -179,6 +391,13 @@ init=function()
     Runtime:addEventListener( "enterFrame", levelLoop )
     -- worldgroup touch eventlistener
     worldgroup:addEventListener("touch", moveWorld)
+	player.spr.touch = playerTouch
+	player.spr:addEventListener("touch", player.spr)
+	--Runtime:addEventListener("collision", onCollide)
+	player.spr.collision = onCollide
+	player.spr:addEventListener("collision", player.spr)
+	player.spr.postCollision = onPostCollide
+	player.spr:addEventListener("postCollision", player.spr)
 end
 
 unloadMe = function()
