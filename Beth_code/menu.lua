07edee06741aad0bc2e55c18f26346881d75912f
unknown <riverj4@rpi.edu>
2011-10-08 16:22:26 -0400

    made a menu module

--- Beth_code/menu.lua
+++ Beth_code/menu.lua
@@ -12,15 +12,18 @@ function menu()
     playButton.id = playnow
     menuGroup:insert(playButton)
     
-    playButtor:addEventListener("touch", init)
+    playButton:addEventListener("touch", init)
 end
 
 function init(event)
     mode = event.target.id
     if mode = playnow then
         menuGroup:removeSelf()
-        exit()
+        unloadMe()
     end
 end
 
+function unloadMe()
+    collectgarbage("collect")
+end
 menu()
