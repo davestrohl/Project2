f4df8f8f191497a3e3784c42b079f90bb62ca034
unknown <riverj4@rpi.edu>
2011-10-08 14:28:04 -0400

    the player and the worldgroup move seperately

--- Beth_code/level1map.lua
+++ Beth_code/level1map.lua
@@ -15,16 +15,7 @@ local world = display.newRect(0,0,1056,960)
 world:setFillColor(128,0,0)
 worldgroup:insert(world)
 
---worldgroup's touch event function
-local moveWorld = function(event)
-    --if event.target == player
-    if event.phase == "ended" then
-        delta_x = event.x - event.xStart
-        delta_y = event.y - event.yStart
-        worldgroup:translate(delta_x, delta_y)
-    end
-        
-end
+
 ---------------------------------------------------------------------
 
 
@@ -106,6 +97,7 @@ local function playerTouch(self, event)
         display.getCurrentStage():setFocus(t)
         t.isFocus = true
         line = nil
+        worldgroup:removeEventListener("touch", moveWorld)
     elseif t.isFocus then
         if event.phase == "moved" then
             if ( line ) then
@@ -121,7 +113,8 @@ local function playerTouch(self, event)
             if ( line ) then
 				line.parent:remove( line )
 			end
-            t:applyForce( (event.x - t.x), (event.y - t.y), t.x, t.y )	
+            t:applyForce( (event.x - t.x), (event.y - t.y), t.x, t.y )
+            worldgroup:addEventListener("touch", moveWorld)
         end
     end
 end
@@ -314,17 +307,17 @@ init=function()
     physics.addBody(right, "static", {bounce =1})
     worldgroup:insert(right)
     --input penguin for testing purposes
-    local penguinSheet = sprite.newSpriteSheet("pixelpenguin.png", 360, 288)
-    local penguinSet = sprite.newSpriteSet(penguinSheet, 1, 2)
-    sprite.add(penguinSet, 1, 2, 400)
-    penguin = sprite.newSprite(penguinSet)
-    penguin.x = 200
-    penguin.y=  500
-    penguin.xScale = .3
-    penguin.yScale = .3
-    penguinpoly = {-30, -70, 30, -70, 70, 0, 40, 60, -40, 60, -70, 0}
-    physics.addBody(penguin, {density = 1.0, friction =10, bounce = 1, shape=penguinpoly})
-    worldgroup:insert(penguin)
+    -- local penguinSheet = sprite.newSpriteSheet("pixelpenguin.png", 360, 288)
+    -- local penguinSet = sprite.newSpriteSet(penguinSheet, 1, 2)
+    -- sprite.add(penguinSet, 1, 2, 400)
+    -- penguin = sprite.newSprite(penguinSet)
+    -- penguin.x = 200
+    -- penguin.y=  500
+    -- penguin.xScale = .3
+    -- penguin.yScale = .3
+    -- penguinpoly = {-30, -70, 30, -70, 70, 0, 40, 60, -40, 60, -70, 0}
+    -- physics.addBody(penguin, {density = 1.0, friction =10, bounce = 1, shape=penguinpoly})
+    -- worldgroup:insert(penguin)
 	
 	local player = Player:new(250, 250)
 	physics.addBody(player.spr, playerBody)
@@ -387,6 +380,17 @@ init=function()
     else
         print("fail")
     end
+    
+    --worldgroup's touch event function
+    local moveWorld = function(event)
+        if event.target ~= player.spr then
+            if event.phase == "ended" then
+                delta_x = event.x - event.xStart
+                delta_y = event.y - event.yStart
+                worldgroup:translate(delta_x, delta_y)
+            end
+        end
+    end
     --local objSheet = sprite.newSpriteSheet("gfx/floor_tile.jpg", 72, 72)
     -- local objSet = sprite.newSpriteSet(objSheet, 1, 1)
     -- sprite.add(objSet, "def", 1, 1, 1000)
@@ -415,10 +419,11 @@ init=function()
 
     -- the screen's main enterFrame event
     Runtime:addEventListener( "enterFrame", levelLoop )
+    player.spr.touch = playerTouch
+	player.spr:addEventListener("touch", player.spr)
     -- worldgroup touch eventlistener
     worldgroup:addEventListener("touch", moveWorld)
-	player.spr.touch = playerTouch
-	player.spr:addEventListener("touch", player.spr)
+	
 	--Runtime:addEventListener("collision", onCollide)
 	player.spr.collision = onCollide
 	player.spr:addEventListener("collision", player.spr)
